bc473f47b9664ef05d0d01b8bdb4898955b473c2
Gabriel Acevedo <gacevedo@gmail.com>
2024-09-24 19:39:41 -0500

    Fixing run.py for finding 2

--- run.py
+++ run.py
@@ -4,11 +4,13 @@ app = create_app()
 
 @app.after_request
 def add_csp_headers(response):
-    # vulnerability: Broken Access Control
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    # vulnerability: Security Misconfiguration
-    response.headers['Content-Security-Policy'] = "script-src 'self' 'unsafe-inline'"
+    # Removed wildcard from CORS header
+    response.headers['Access-Control-Allow-Origin'] = 'trusted-domain.com'
+    # Removed 'unsafe-inline' from CSP header
+    response.headers['Content-Security-Policy'] = "script-src 'self'"
     return response
+
 
 if __name__ == '__main__':
     app.run()
+
